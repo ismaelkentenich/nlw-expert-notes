d123ec538b89bada39b55624cbbe82e7a2fe8d71
Ismael Kentenich <ismaelkentenichm@gmail.com>
2024-02-06 10:54:49 -0300

    feat: add dynamics to note-card

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,16 +1,19 @@
-export function NoteCard () {
+interface NoteCardProps {
+    note: {
+        date: Date
+        content: string
+    }
+}
+
+export function NoteCard ({note}: NoteCardProps) {
     return (
-        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none
+        <button className="rounded-md text-left flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none
                 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-            <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+            <span className="text-sm font-medium text-slate-300">
+                {note.date.toISOString()}
+            </span>
             <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-            corporis nobis adipisci autem eligendi impedit voluptas illum
-            quidem. Delectus aut ratione laboriosam veniam iusto. Adipisci non
-            vitae ipsam dolores iusto! Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Eaque corporis nobis adipisci autem eligendi
-            impedit voluptas illum quidem. Delectus aut ratione laboriosam
-            veniam iusto. Adipisci non vitae ipsam dolores iusto!
+                {note.content}
             </p>
             <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0
                 pointer-events-none" />
