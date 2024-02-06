035f30da2aea7379005b6dfdc079dcf31ae0f04f
Ismael Kentenich <ismaelkentenichm@gmail.com>
2024-02-06 11:35:39 -0300

    feat: create modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,22 +1,46 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
 interface NoteCardProps {
-    note: {
-        date: Date
-        content: string
-    }
+  note: {
+    date: Date;
+    content: string;
+  };
 }
 
-export function NoteCard ({note}: NoteCardProps) {
-    return (
-        <button className="rounded-md text-left flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none
-                hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-            <span className="text-sm font-medium text-slate-300">
-                {note.date.toISOString()}
-            </span>
-            <p className="text-sm leading-6 text-slate-400">
-                {note.content}
-            </p>
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0
-                pointer-events-none" />
-      </button>
-    )
+export function NoteCard({ note }: NoteCardProps) {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger
+        className="rounded-md text-left flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none
+                    hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400"
+      >
+        <span className="text-sm font-medium text-slate-300">
+            {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+        <div
+          className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0
+                        pointer-events-none"
+        />
+      </Dialog.Trigger>
+
+      {/* When used, portals your overlay and content parts into the body. */}
+      <Dialog.Portal>
+        {/* A layer that covers the inert portion of the view when the dialog is open. */}
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.DialogContent className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh]
+            bg-slate-700 rounded-md flex flex-col outline-none">
+            <div className="flex flex-1 flex-col gap-3 p-5">
+                <span className="text-sm font-medium text-slate-300">
+                    {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})}
+                </span>
+                <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+
+            </div>
+        </Dialog.DialogContent>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
 }
