761acd2f39a38b63cc7d3f8e0f553ca4e84592df
Ismael Kentenich <ismaelkentenichm@gmail.com>
2024-02-06 09:45:15 -0300

    feat: add card hover effect

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,7 @@
 export function NoteCard () {
     return (
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none
+                hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
             <span className="text-sm font-medium text-slate-300">há 2 dias</span>
             <p className="text-sm leading-6 text-slate-400">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
@@ -11,7 +12,8 @@ export function NoteCard () {
             impedit voluptas illum quidem. Delectus aut ratione laboriosam
             veniam iusto. Adipisci non vitae ipsam dolores iusto!
             </p>
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-      </div>
+            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0
+                pointer-events-none" />
+      </button>
     )
 }
