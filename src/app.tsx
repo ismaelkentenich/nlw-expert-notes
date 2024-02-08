8e1033256bd9414ad614beb28002d5d0dae2de82
Ismael Kentenich <ismaelkentenichm@gmail.com>
2024-02-08 10:26:01 -0300

    feat: add delete note function

--- src/app.tsx
+++ src/app.tsx
@@ -36,6 +36,15 @@ export function App() {
     localStorage.setItem("notes", JSON.stringify(notesArray));
   }
 
+  function onNoteDeleted(id:string) {
+    const notesArray = notes.filter(note => {
+      return note.id != id
+    })
+
+    setNotes(notesArray)
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
   function handleSearch(event: ChangeEvent<HTMLInputElement>){
     const query = event.target.value 
 
@@ -67,7 +76,7 @@ export function App() {
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {filteredNotes.map((note) => {
-          return <NoteCard key={note.id} note={note} />;
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/>;
         })}
       </div>
     </div>
